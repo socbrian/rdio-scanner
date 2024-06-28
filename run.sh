30a38e6f2a4ea07875d3bdacd5b3b1dab217f0ee
socbrian <socbrian@users.noreply.github.com>
2024-06-28 14:14:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,3 @@
 
 # Build the Docker image (replace with your actual path and tag)
 docker build -t Dockerfile
-
-#run
-docker run --detach --env TZ=America/NewYork --name rdio-scanner --publish 3000:3000 --restart always  --volume ~/.rdio-scanner:/app/data Dockerfile
